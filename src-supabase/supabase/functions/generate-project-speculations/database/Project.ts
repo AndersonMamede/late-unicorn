85756dac390fdf0a6925b0d31202eccedc6fbc59
Jefferson <jeffersongibin@yahoo.com.br>
2024-04-18 15:46:14 -0300

    create function sentences generations

---
+++ src-supabase/supabase/functions/generate-project-speculations/database/Project.ts
@@ -0,0 +1,27 @@
+import { SupabaseClient } from "./supabaseClient.ts";
+
+export interface IProject {
+  project_id: string;
+  name: string;
+  description: string;
+};
+
+const getById = async (
+  projectId: string,
+): Promise<IProject | null> => {
+  const { data, error } = await SupabaseClient()
+    .from("project")
+    .select("project_id, name, description").eq("project_id", projectId).limit(
+      1,
+    );
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data && data.length > 0 ? data[0] : null;
+};
+
+export const Project = {
+  getById,
+};
--- src-supabase/supabase/functions/generate-project-speculations/index.ts
+++ src-supabase/supabase/functions/generate-project-speculations/index.ts
@@ -1,32 +1,66 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { generateSentences } from "./integration/generateSentences.ts";
+import { Project } from "./database/Project.ts";
+import { ProjectSpeculation } from "./database/ProjectSpeculation.ts";
+import { payloadValidation } from "./payloadValidation.ts";
 
-// Setup type definitions for built-in Supabase Runtime APIs
-/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST",
+};
 
-console.log("Hello from Functions!")
+const handleRequest = async (req: Request) => {
+  try {
+    if (req.method !== "POST") {
+      return new Response(null, {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 405,
+      });
+    }
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+    const { projectId } = await req.json();
+    const { passes, errors } = await payloadValidation({
+      method: req.method,
+      projectId,
+    });
+
+    if (!passes) {
+      return new Response(
+        JSON.stringify({ errors }),
+        {
+          headers: { ...corsHeaders, "Content-Type": "application/json" },
+          status: 400,
+        },
+      );
+    }
+
+    const project = await Project.getById(projectId);
+
+    if (!project) {
+      return new Response(null, {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 406,
+      });
+    }
+
+    const { sentences } = await generateSentences({ project });
+    await ProjectSpeculation.create({ project, sentences });
+
+    return new Response(
+      JSON.stringify({ sentences }),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200,
+      },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: error.message }),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 400,
+      },
+    );
   }
+};
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/generate-project-speculations' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+Deno.serve(handleRequest);
